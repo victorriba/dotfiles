3f935bf9c5a7c640cb5185b084c0f6c6a523af14
victorriba <victorriba18@hotmail.com>
2024-07-26 19:36:59 -0500

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,14 +8,26 @@ local config = {
                 bottom = 0,
         },
         window_decorations = "RESIZE",
-        font_size = 16.0,
+	font = wezterm.font("MesloLGS Nerd Font Mono"),
+	font_size = 19.0,
         tab_max_width = 22,
         audible_bell = "Disabled",
         window_close_confirmation = "NeverPrompt",
         use_fancy_tab_bar = false,
-	window_background_opacity = 0.95,
-	send_composed_key_when_left_alt_is_pressed = true,
+	window_background_opacity = 0.9,
+	macos_window_background_blur = 10,
 
+}
+config.colors = {
+	foreground = "#CBE0F0",
+	background = "#011423",
+	cursor_bg = "#47FF9C",
+	cursor_border = "#47FF9C",
+	cursor_fg = "#011423",
+	selection_bg = "#033259",
+	selection_fg = "#CBE0F0",
+	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
 }
 config.keys = {}
 for i = 1, 8 do
