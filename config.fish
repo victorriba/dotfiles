21959c0dee5276ed9d44ec7fc22e9248dd9163e0
victorriba <victorriba18@hotmail.com>
2024-07-31 18:47:50 -0500

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,5 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set -x PATH $PATH (go env GOPATH)/bin
+    set -x PATH /usr/local/opt/ruby/bin $PATH
+    set -x ANDROID_HOME $HOME/Library/Android/sdk
+    set -x PATH $PATH $ANDROID_HOME/emulator
+    set -x PATH $PATH $ANDROID_HOME/tools
+    set -x PATH $PATH $ANDROID_HOME/tools/bin
+    set -x PATH $ANDROID_HOME/platform-tools $PATH
     zoxide init fish --cmd cd | source
 end
 
